9af1b9d9a45b5951e2d0d6079df335c8987d08a3
Morxidia <morxidia@gmail.com>
2026-05-10 08:51:38 +0700

    add: add multithread page layout, not done yet

---
+++ MainApp/src/mainapp/ui/MultiThreadPage.java
@@ -0,0 +1,16 @@
+package mainapp.ui;
+/**
+ * @author morxidia
+ * this page show the main default branch output as text in java swing UI
+ */
+
+import javax.swing.JPanel;
+import java.awt.Dimension;
+
+public class MultiThreadPage extends JPanel {
+    public MultiThreadPage() {
+        setLayout(null); // Keep your absolute layout for now
+        setPreferredSize(new Dimension(910, 500)); // Set a size that fits the tab
+        
+    }
+}
