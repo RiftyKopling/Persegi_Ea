eb84d874459d96bcb83f8b4703282d48cf9fdc01
RiftyKopling <arsy1429@gmail.com>
2026-05-14 20:22:46 +0700

    fiks banget banget inimah kudune

--- MainApp/src/mainapp/ui/HomePanel.java
+++ MainApp/src/mainapp/ui/HomePanel.java
@@ -129,6 +129,7 @@ public class HomePanel extends JPanel {
         //Schedule a job for the event dispatch thread:
         //creating and showing this application's GUI.
         SwingUtilities.invokeLater(new Runnable() {
+            @Override
             public void run() {
                 //Turn off metal's use of bold fonts
 		UIManager.put("swing.boldMetal", Boolean.FALSE);
